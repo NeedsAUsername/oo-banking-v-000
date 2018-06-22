b6a6f21c4e59c16d03e61fdb17465865e8c7cc56
Kevin Chi <kevinchi118@gmail.com>
2018-06-22 17:41:25 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -18,9 +18,6 @@ class BankAccount
     end
 
     def valid?
-        if self.status == "open" && self.balance > 0
-            true
-        else false
-        end
-    end 
+        self.status == "open" && self.balance > 0?
+    end
 end
